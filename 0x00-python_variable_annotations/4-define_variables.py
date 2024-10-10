3ff5952745eb6c1194fda243edcd10c01aefe69e
MohamedYehia20m <medoyehia2001@gmail.com>
2024-10-10 12:06:40 +0000

    add Task4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python3
+"""
+This module demonstrates the use of variable annotations in Python.
+
+The module defines four variables with their types explicitly annotated:
+    - `a`: An integer.
+    - `pi`: A floating-point number representing the value of π (pi).
+    - `i_understand_annotations`: A boolean indicating whether the user
+    understands annotations.
+    - `school`: A string representing the name of a school.
+
+Variables:
+    - a (int): An integer value.
+    - pi (float): A floating-point value representing π.
+    - i_understand_annotations (bool): A boolean indicating
+    understanding of annotations.
+    - school (str): A string representing the name of the school
+    ("Holberton").
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
